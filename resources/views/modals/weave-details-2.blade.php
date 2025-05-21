83d5a94ca5ceca58c95b39b1952bbba0ad489c45
DucMatt07 <142436621+DucMatt07@users.noreply.github.com>
2025-05-21 23:27:52 +0700

    delete-form,fabrication,return,view-dye,weave-details

---
+++ resources/views/modals/weave-details-2.blade.php
@@ -0,0 +1,96 @@
+<div class="w-[1100px] h-[445px] flex flex-wrap gap-y-[10px] justify-end px-[8px] py-[13px]  mx-auto my-[20px] shadow rounded-[10px]">
+    <div class="w-full h-[10%] items-center flex justify-between px-[20px] py-[10px] rounded-[10px] bg-[#fff]  shadow ">
+        <p class="text-[14px] text-[#000]">Weave  Details</p>
+        <i class="fa-solid fa-xmark text-[12px] text-[#D93F21]"></i>
+    </div>
+    <div class="w-full flex flex-wrap gap-x-[10px] p-[20px] rounded-[10px] bg-[#fff] shadow text-[12px]">
+        <div class="w-[15%] h-[65px]">
+            <label for=""> Supplier</label>
+        <input class="w-full bg-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Sameer" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Supplier Code</label>
+    <input class="w-full bg-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="SP-0032" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Item</label>
+    <input class="w-full bg-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Yarn 001" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>  <div class="w-[15%] h-[65px]">
+        <label for=""> Return Quantity</label>
+    <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+    </div>
+    <div class="relative w-[23%] h-[65px]">
+        <label for=""> Return Quantity</label>
+        <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+      </div>   
+      <div class="relative w-[23%] h-[65px]">
+        <label for=""> Return Quantity</label>
+        <input class="w-full bg-[#fff] border-[1px] border-[#D9D9D9] p-[10px] rounded-[5px] placeholder-[#A5A1A1]" placeholder="Kg" type="text">
+      </div>   
+      <div class="relative w-[23%] h-[65px]">
+        <label for="">Supplier</label>
+        <select
+          class="w-full border border-[#D9D9D9] p-[10px] pr-[36px] rounded-[5px] text-[#A5A1A1] appearance-none">
+          <option selected>Select Supplier</option>
+        </select>
+        <div class="pointer-events-none absolute right-[12px] top-[58%] transform -translate-y-1/2">
+          <svg class="w-4 h-4 text-[#A5A1A1]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+          </svg>
+        </div>
+      </div>   
+      <div class="relative w-[23%] h-[65px]">
+        <label for="">Supplier</label>
+        <select
+          class="w-full border border-[#D9D9D9] p-[10px] pr-[36px] rounded-[5px] text-[#A5A1A1] appearance-none">
+          <option selected>Select Supplier</option>
+        </select>
+        <div class="pointer-events-none absolute right-[12px] top-[58%] transform -translate-y-1/2">
+          <svg class="w-4 h-4 text-[#A5A1A1]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+          </svg>
+        </div>
+      </div>   
+    
+   
+    </div>
+    <div class="h-[30%] w-full flex justify-end flex-wrap">
+        <div class="">
+<label for="message" class="block  mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+<textarea id="message" rows="4" class="block p-2.5 w-[406px] text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+</div>
+    </div>
+    <button class="h-[10%] py-[11px] pl-[20px] pr-[24px] text-[#fff] text-[14px] font-semibold text-center rounded-[10px] bg-[#BE202F] mt-[10px] self-end">Save</button>
+    </div>
