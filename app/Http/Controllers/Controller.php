462fef35a14fac59c5067954939622fdba1e6fa5
Windows 11 <thaidvph50988@gmail.com>
2025-05-22 22:38:55 +0700

    baseRepository

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,49 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
+
 abstract class Controller
 {
-    
+
+    protected $handleRepository;
+
+    // handle upload only one file
+    public function handleUploadOneFile(object $file)
+    {
+        $path = null;
+        if ($file !== null) {
+            $file_name = time() . '_' . $file->getClientOriginalName();
+            $path = $file->storeAs('uploads', $file_name, 'public');
+        }
+        return $path;
+    }
+    // handle upload mutil file
+    public function handleUploadMutilFile(array $files)
+    {
+        $path = [];
+        if (!empty($files)) {
+            foreach ($files as $key => $file) {
+                $path[] = $this->handleUploadOneFile($file);
+            }
+        }
+    }
+    // chỉ cần truyền file mới và đường đẫn file cũ sẽ xử lý để xoá cũ thêm mới trả về đường dẫn file mới
+    public function handleUpdateFile(object $file, string $old_file)
+    {
+        if ($file != null) {
+            if (isset($old_file) && Storage::disk('public')->exists($old_file)) {
+                $this->deleteFile($old_file);
+            }
+            return $this->handleUploadOneFile($file);
+        }
+        return $old_file;
+    }
+    public function deleteFile(string $path)
+    {
+        if (isset($path) && Storage::disk('public')->exists($path)) {
+            return Storage::disk('public')->delete($path);
+        }
+        return false;
+    }
 }
