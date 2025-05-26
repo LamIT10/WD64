95296fbc329376a4f08644df3f8a55bcbecfc8e0
NguyenLoc2105 <146914879+NguyenLoc2105@users.noreply.github.com>
2025-05-26 23:31:22 +0700

    delete file

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::prefix('admin')->as('admin.')->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/dashboard', function () {  
         return Inertia::render('Dashboard');
     });
 });
