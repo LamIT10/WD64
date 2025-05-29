1a004e6543ddc1c32ea3be61b41c0dde379f0348
Hoantofff <hoannqph49532@gmail.com>
2025-05-29 21:46:05 +0700

    add category demo

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\SupplierController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,4 +12,9 @@ Route::prefix('admin')->as('admin.')->group(function () {
     });
     Route::resource('categories', CategoryController::class);
     Route::resource('products', ProductController::class);
+    Route::group(['prefix' => 'suppliers', 'as' => 'suppliers.'], function () {
+        Route::get('/', [SupplierController::class, 'getList'])->name('index');
+        Route::get('create', [SupplierController::class, 'create'])->name('create');
+        Route::get('store', [SupplierController::class, 'store'])->name('store');
+    });
 });
