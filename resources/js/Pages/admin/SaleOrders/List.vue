d61f94bc59a923da001170c37791ad3d1b7b661b
DucMatt07 <142436621+DucMatt07@users.noreply.github.com>
2025-07-31 23:19:10 +0700

    fix QR code not in cancelled, completed in sale orders

--- resources/js/Pages/admin/SaleOrders/List.vue
+++ resources/js/Pages/admin/SaleOrders/List.vue
@@ -792,6 +792,11 @@
                                     Xác nhận hoàn thành
                                 </button>
                                 <button
+                                    v-if="
+                                        !['cancelled', 'completed'].includes(
+                                            selectedOrder.status
+                                        )
+                                    "
                                     @click="generateQR(selectedOrder.id)"
                                     type="button"
                                     class="mt-3 w-full flex shadow-xl justify-center gap-1 items-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-gray-600 text-base font-medium text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
